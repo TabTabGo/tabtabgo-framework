e1ffa670c1b44cf4752552a0b648d93ac6ec38a3
Ghassan Samara <ghassan@tabtabgo.com>
2021-02-10 19:29:02 +0400

    fix(web): convert Validator to function component
    BREAKING CHANGE: convert Validator to functional component

--- packages/web/src/components/Validations/Validator.js
+++ packages/web/src/components/Validations/Validator.js
@@ -1,119 +1,77 @@
 /* eslint-disable no-console */
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import * as StandardValidations from '@tabtabgo/core/Validations.js';
 import * as StandardValidationsErrors from '@tabtabgo/core/ValidationErrors.js';
 import withValidation from './withValidation';
-import { withComponentProvider } from '@tabtabgo/core';
+import { ValidationFormContext } from './Form';
+import { ComponentContext } from '@tabtabgo/core';
 import { getInputValue, isEmpty, getFriendlyString } from '@tabtabgo/core/Utilities';
 import _ from 'lodash';
 
-class Validator extends Component {
-  static propTypes = {
-    classes: PropTypes.object,
-    styles: PropTypes.object,
-    id: PropTypes.string,
-    name: PropTypes.string,
-    label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-    placeholder: PropTypes.string,
-    validation: PropTypes.shape({
-      isDirty: PropTypes.bool,
-      isValid: PropTypes.bool,
-      validationErrors: PropTypes.array,
-      setValidationState: PropTypes.func,
-      registerValidationInput: PropTypes.func,
-    }),
-    validations: PropTypes.object,
-    validationErrors: PropTypes.object,
-    onValidate: PropTypes.func,
-    isRequired: PropTypes.bool,
-    value: PropTypes.any,
-    originalValue: PropTypes.any,
-    onChange: PropTypes.func.isRequired,
-    onBlur: PropTypes.func,
-    form: PropTypes.node,
-    component: PropTypes.oneOfType([PropTypes.func, PropTypes.string, PropTypes.object]).isRequired,
-    children: PropTypes.node,
-    textFieldProps: PropTypes.object,
-    ignoreLabelSuffix: PropTypes.bool,
-    error: PropTypes.bool,
-    helperText: PropTypes.string,
-    inputComponentRef: PropTypes.func,
-    ignoreRegisteredValidation: PropTypes.bool,
-    componentProvider: PropTypes.shape({
-      getFormInputLabel: PropTypes.func,
-    }),
-  };
+const iniValidatorState = {
+  originalValue: undefined,
+  value: undefined,
+  validations: {},
+  validationErrors: {},
+  isValid: true,
+  isDirty: false,
+  isTouched: false,
+  validationError: '',
+}
 
-  constructor(props) {
-    super(props);
-    let initState = {
-      originalValue: null,
-      validations: {},
-      validationErrors: {},
-      isValid: true,
-      isDirty: false,
-      isTouched: false,
-      validationError: '',
-    };
-    this.isRegisteredToForm = false;
-    this.state = this.setInitValidationState(initState, props);
-  }
-
-  static getDerivedStateFromProps(props, state) {
+function Validator(props) {
+  const [state, setState] = useState(setInitValidationState(initState, props));
+  const validationCtx = useContext(ValidationFormContext);
+  const componentContext = useContext(ComponentContext);
+  useEffect(() => {
     if (!_.isEqual(state.originalValue, props.originalValue)) {
-      return {
-        originalValue: props.originalValue,
-      };
+      setState({ ...state, originalValue: props.originalValue });
     }
+  }, [props.originalValue]);
 
-    return null;
-  }
-
-  async componentDidUpdate(prevProps) {
+  useEffect(() => {
     const {
       name,
-      validation: { setValidationState },
       isRequired,
-    } = this.props;
-    let newValue = this.getValue(this.props);
-    let oldValue = this.getValue(prevProps);
+    } = props;
+    let newValue = getValue(props);
+    let oldValue = getValue(state);
     if (!_.isEqual(newValue, oldValue)) {
       //console.log("validation :", name, newValue, oldValue);
-      await this.validateInput(newValue, name, setValidationState, isRequired);
+      await validateInput(newValue, name, validationCtx.setValidationState, isRequired);
     }
     //Logically not required
-    if (!_.isEqual(prevProps.originalValue, this.props.originalValue)) {
+    if (!_.isEqual(state.originalValue, props.originalValue)) {
       let newState = {
-        isDirty: !_.isEqual(this.props.originalValue, newValue),
-        originalValue: this.props.originalValue,
+        isDirty: !_.isEqual(props.originalValue, newValue),
+        originalValue: props.originalValue,
       };
       // console.group("componentDidUpdate");
       // console.log("value", newValue);
-      // console.log("original", this.props.originalValue);
+      // console.log("original", props.originalValue);
       // console.log("newState :", newState);
       // console.groupEnd();
-      this.setState(newState);
+      setState(newState);
     }
-  }
+  }, [props, props.name, props.isRequired])
 
-  componentDidMount() {
+  useEffect(() => {
     const {
       name,
       id,
       isRequired,
-      validation: { registerValidationInput },
       inputComponentRef,
-    } = this.props;
+    } = props;
 
-    if (registerValidationInput) {
-      registerValidationInput(name ? name : id, {
+    if (validationCtx?.registerValidationInput) {
+      validationCtx.registerValidationInput(name ? name : id, {
         isRequired,
         validate: async () => {
-          const { name, isRequired } = this.props;
-          const value = this.getValue(this.props);
+          const { name, isRequired } = props;
+          const value = getValue(props);
           //console.log("Run input Validate", value);
-          return await this.validateInput(value, name, null, isRequired);
+          return await validateInput(value, name, null, isRequired);
         },
       });
     }
@@ -121,17 +79,18 @@ class Validator extends Component {
     if (inputComponentRef) {
       inputComponentRef(this);
     }
-  }
+  }, [])
+
   /*
   Get the Value passed to input and can be set directly or by InputProps
   */
-  getValue = (props) => {
-    const { value } = props;
+  const getValue = (privateProps) => {
+    const { value } = privateProps;
     return value;
   };
 
-  getPlaceholder = (props) => {
-    const { name, id } = props;
+  const getPlaceholder = (privateProps) => {
+    const { name, id } = privateProps;
 
     //if (placeholder) return placeholder;
     if (name) return getFriendlyString(name);
@@ -140,9 +99,9 @@ class Validator extends Component {
     return 'This ';
   };
 
-  parseValidations = (props) => {
+  const parseValidations = (privateProps) => {
     var newValidations = {};
-    const { validations, isRequired } = props;
+    const { validations, isRequired } = privateProps;
 
     if (validations)
       for (let key in validations) {
@@ -173,9 +132,9 @@ class Validator extends Component {
     return newValidations;
   };
 
-  parseValidationErrors = (props) => {
+  const parseValidationErrors = (privateProps) => {
     var newValidationsErrors = {};
-    const { validations, validationErrors, isRequired } = props;
+    const { validations, validationErrors, isRequired } = privateProps;
 
     if (validations) {
       for (let key in validations) {
@@ -188,11 +147,11 @@ class Validator extends Component {
             if (typeof validation !== 'boolean') {
               newValidationsErrors[key] = StandardValidationsErrors[key](
                 validation,
-                this.getPlaceholder(this.props),
+                getPlaceholder(props),
               );
             } else {
               newValidationsErrors[key] = StandardValidationsErrors[key](
-                this.getPlaceholder(this.props),
+                getPlaceholder(props),
               );
             }
           }
@@ -208,31 +167,30 @@ class Validator extends Component {
     } else {
       newValidationsErrors = {
         ...newValidationsErrors,
-        required: StandardValidationsErrors.required(this.getPlaceholder(props)),
+        required: StandardValidationsErrors.required(getPlaceholder(props)),
       };
     }
     return newValidationsErrors;
   };
 
-  setInitValidationState = (state, props) => {
-    let validations = this.parseValidations(props);
-    let validationErrors = this.parseValidationErrors(props);
-    return Object.assign({}, state, { validations, validationErrors });
+  const setInitValidationState = (internalState, internalProps) => {
+    let validations = parseValidations(internalProps);
+    let validationErrors = parseValidationErrors(internalProps);
+    return Object.assign({}, internalState, { validations, validationErrors });
   };
 
-  handleChange = (newValue, event) => {
-    const { onChange } = this.props;
+  const handleChange = (newValue, event, sname) => {
+    const { onChange } = props;
     if (onChange) onChange(event ? event : newValue);
   };
   //TODO should validate onBlur first time when value is empty only ?.
-  handleBlur = async (newValue, event, name) => {
+  const handleBlur = async (newValue, event, sName) => {
     const {
       onBlur,
-      isRequired,
-      validation: { setValidationState },
-    } = this.props;
+      isRequired
+    } = props;
 
-    await this.validateInput(newValue, name, setValidationState, isRequired);
+    await validateInput(newValue, sName, validationCtx.setValidationState, isRequired);
     if (onBlur) {
       onBlur(event ? event : newValue);
     }
@@ -240,22 +198,22 @@ class Validator extends Component {
     return true;
   };
 
-  validateInput = async (newValue, name, setFormState, isRequired = false, dirtyState = null) => {
+  const validateInput = async (newValue, sName, setFormState, isRequired = false, dirtyState = null) => {
     let newState = dirtyState || {
-      isDirty: !_.isEqual(this.state.originalValue, newValue),
+      isDirty: !_.isEqual(state.originalValue, newValue),
       isValid: true,
       validationError: '',
     };
 
     //if (newState.isDirty === true)
     //{
-    if (this.props.error === true) {
+    if (props.error === true) {
       newState = Object.assign({}, newState, {
         isValid: false,
-        validationError: this.props.helperText,
+        validationError: props.helperText,
       });
     } else {
-      let validationResult = await this.validate(newValue, name, isRequired);
+      let validationResult = await validate(newValue, sName, isRequired);
 
       newState = Object.assign({}, newState, validationResult);
     }
@@ -265,13 +223,13 @@ class Validator extends Component {
     //   console.group("validateInput");
     //   console.log("input Name", name);
     //   console.log("value", newValue);
-    //   console.log("original", this.state.originalValue);
+    //   console.log("original", state.originalValue);
     //   console.log("newState", newState);
     //   console.groupEnd();
     // }
 
     // set new inputIsValid
-    this.setState(newState);
+    setState(newState);
     //Update form isDirty and isValidate
     if (setFormState) {
       setFormState(name, newState);
@@ -280,12 +238,12 @@ class Validator extends Component {
   };
 
   /// Check is input is dirty
-  isDirty = () => this.state.isDirty;
+  const isDirty = () => state.isDirty;
   // Check is input valid
-  isValid = () => this.state.isValid;
+  const isValid = () => state.isValid;
   // Get error message if input is not Valid
-  error = () => this.state.error;
-  validatingInput = (result, newState, validationError) => {
+  const error = () => state.error;
+  const validatingInput = (result, newState, validationError) => {
     if (result) {
       newState.isValid = true;
       newState.validationError = null;
@@ -298,16 +256,16 @@ class Validator extends Component {
   };
 
   //Validate value based on register validation
-  validate = async (newValue, name, isRequired) => {
+  const validate = async (newValue, name, isRequired) => {
     let newState = {
       isValid: true,
       validationError: '',
     };
-    let validations = this.state.validations;
-    let validationErrors = this.state.validationErrors;
-    if (this.props.ignoreRegisteredValidation) {
-      validations = this.parseValidations(this.props);
-      validationErrors = this.parseValidationErrors(this.props);
+    let validations = state.validations;
+    let validationErrors = state.validationErrors;
+    if (props.ignoreRegisteredValidation) {
+      validations = parseValidations(props);
+      validationErrors = parseValidationErrors(props);
     }
 
     for (let key in validations) {
@@ -321,7 +279,7 @@ class Validator extends Component {
               newState.isValid = false;
               newState.validationError = 'Validating...';
               let result = await validationResult;
-              newState = this.validatingInput(result, newState, validationErrors[key]);
+              newState = validatingInput(result, newState, validationErrors[key]);
               if (newState.isValid === false) {
                 break;
               }
@@ -332,7 +290,7 @@ class Validator extends Component {
               //console.log("Failed to validate error :", error);
             }
           } else {
-            newState = this.validatingInput(validationResult, newState, validationErrors[key]);
+            newState = validatingInput(validationResult, newState, validationErrors[key]);
             if (newState.isValid === false) {
               break;
             }
@@ -340,27 +298,45 @@ class Validator extends Component {
         } else if (isRequired && isEmpty(newValue)) {
           newState.isValid = false;
           newState.validationError = StandardValidationsErrors.required(
-            this.getPlaceholder(this.props),
+            getPlaceholder(props),
           );
           break;
         }
       }
     }
     //console.log("newState :", name, newValue,  newState);
-    if (this.props.onValidate) {
-      this.props.onValidate(newState.isValid, newState.validationError);
+    if (props.onValidate) {
+      props.onValidate(newState.isValid, newState.validationError);
     }
 
     return newState;
   };
 
-  render() {
+  const getLabel = (internalProps) => {
+    const { label, textFieldProps } = internalProps;
+    let strLabel = label;
+
+    if (textFieldProps && textFieldProps.label) {
+      strLabel = componentContext.getFormInputLabel({
+        isRequired,
+        ignoreLabelSuffix,
+        label: textFieldProps.label,
+      });
+    } else if (label) {
+      strLabel = componentContext.getFormInputLabel({
+        isRequired,
+        ignoreLabelSuffix,
+        label,
+      });
+    }
+    return strLabel;
+  }
+
+  const render = (internalProps) => {
     const {
       name,
       id,
       component,
-      // eslint-disable-next-line no-unused-vars
-      isRequired,
       // eslint-disable-next-line no-unused-vars
       originalValue,
       // eslint-disable-next-line no-unused-vars
@@ -377,35 +353,12 @@ class Validator extends Component {
       ignoreLabelSuffix,
       // eslint-disable-next-line no-unused-vars
       ignoreRegisteredValidation,
-      componentProvider,
       ...rest
-    } = this.props;
+    } = internalProps;
 
-    let currentValue = this.getValue(this.props);
+
     const Component = component;
-
-    if (currentValue === undefined) {
-      //console.log("name of undefined value", name);
-    }
-
-    let handleChange = (event) => this.handleChange(getInputValue(event), event, name ? name : id);
-    let handleBlur = (event) => this.handleBlur(getInputValue(event), event, name ? name : id);
-
-    let finalLabel = label;
-
-    if (textFieldProps && textFieldProps.label) {
-      finalLabel = componentProvider.getFormInputLabel({
-        isRequired,
-        ignoreLabelSuffix,
-        label: textFieldProps.label,
-      });
-    } else if (label) {
-      finalLabel = componentProvider.getFormInputLabel({
-        isRequired,
-        ignoreLabelSuffix,
-        label,
-      });
-    }
+    const finalLabel = getLabel(internalProps);
 
     //Set validator props
     let validatorProps = {};
@@ -414,28 +367,56 @@ class Validator extends Component {
     }
 
     // console.log("name :", name);
-    // console.log("this.state.isValid :", this.state.isValid);
-    // console.log("this.state.isDirty :", this.state.isValid);
-    // console.log("this.state.validationError :", this.state.validationError);
+    // console.log("state.isValid :", state.isValid);
+    // console.log("state.isDirty :", state.isValid);
+    // console.log("state.validationError :", state.validationError);
     return (
       <Component
         {...rest}
-        //success={this.state.isValid && this.state.isDirty}
-        error={!this.state.isValid}
-        label={finalLabel}
-        helperText={this.state.validationError ? this.state.validationError : helperText}
+        //success={state.isValid && state.isDirty}
+        error={!state.isValid}
+        label={getLabel()}
+        helperText={state.validationError ? state.validationError : helperText}
         id={id}
         name={name}
-        onChange={handleChange}
-        value={currentValue}
-        onBlur={handleBlur}
+        onChange={(event) => handleChange(getInputValue(event), event, name ? name : id)}
+        value={getValue(internalProps)}
+        onBlur={(event) => handleBlur(getInputValue(event), event, name ? name : id)}
         {...validatorProps}
-      //onFocus={event => this.handleFocus(event, name ? name : id)}
+      //onFocus={event => handleFocus(event, name ? name : id)}
       >
-        {this.props.children}
+        {internalProps.children}
       </Component>
     );
   }
+
+  return render();
 }
 
-export default withValidation(withComponentProvider(Validator));
+Validator.propTypes = {
+  classes: PropTypes.object,
+  styles: PropTypes.object,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  placeholder: PropTypes.string,
+  validations: PropTypes.object,
+  validationErrors: PropTypes.object,
+  onValidate: PropTypes.func,
+  isRequired: PropTypes.bool,
+  value: PropTypes.any,
+  originalValue: PropTypes.any,
+  onChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func,
+  form: PropTypes.node,
+  component: PropTypes.oneOfType([PropTypes.func, PropTypes.string, PropTypes.object]).isRequired,
+  children: PropTypes.node,
+  textFieldProps: PropTypes.object,
+  ignoreLabelSuffix: PropTypes.bool,
+  error: PropTypes.bool,
+  helperText: PropTypes.string,
+  inputComponentRef: PropTypes.func,
+  ignoreRegisteredValidation: PropTypes.bool,
+};
+
+export default Validator;
